ef789d0535b47490a4183d9c636d5929e559b88f
Kamil Kuryś <kkurys@10g.pl>
2016-12-14 17:43:52 +0100

    added admin panel and cart

---
+++ Shop/Models/CartViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace Shop.Models
+{
+    public class CartViewModel : BaseViewModel
+    {
+        private double _totalSum = 0;
+        private List<Product> _products;
+        public CartViewModel(List<Product> cart)
+        {
+            if (cart != null)
+            {
+                _products = cart;
+                foreach (Product p in _products)
+                {
+                    _totalSum += double.Parse(p.ActualPrice.ToString());
+                }
+            }
+        }
+        public List<Product> Products
+        {
+            get
+            {
+                if (_products == null)
+                {
+                    return new List<Product>();
+                }
+                else
+                {
+                    return _products;
+                }
+            }
+        }
+        public double Total
+        {
+            get
+            {
+                return _totalSum;
+            }
+        }
+    }
+}
