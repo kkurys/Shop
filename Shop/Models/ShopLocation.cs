f9e43d195950a68a84ad15ab39ae8377c73a8dcd
Kamil Kuryś <kkurys@10g.pl>
2016-12-15 19:40:20 +0100

    fixed some language mistakes

--- Shop/Models/ShopLocation.cs
+++ Shop/Models/ShopLocation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Shop.Models
 {
@@ -7,12 +8,19 @@ namespace Shop.Models
     {
         //...
         public int ShopLocationID { get; set; }
+        [Display(Name = "Godzina otwarcia")]
         public DateTime OpeningHour { get; set; }
+        [Display(Name = "Godzina zamknięcia")]
         public DateTime ClosingHour { get; set; }
+        [Display(Name = "Telefon")]
         public string Phone { get; set; }
+        [Display(Name = "Email")]
         public string EMail { get; set; }
+        [Display(Name = "Miasto")]
         public string City { get; set; }
+        [Display(Name = "Ulica i numer lokalu")]
         public string FullAddress { get; set; }
+        [Display(Name = "Kod pocztowy")]
         public string PostalCode { get; set; }
         public virtual ICollection<Employee> Employees { get; set; }
     }
